ee6136b4341e96721e5455150f54a6467654a9f3
Jonathan Mah <jon@bobopro.local>
2025-05-12 19:21:25 -0700

    init

---
+++ client.cpp
@@ -0,0 +1,67 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include <thread>
+
+constexpr u_int16_t SERVER_PORT = 8080;
+
+void receiveMessages(int client_fd) {
+    char buffer[1024]; 
+    int bytes_received;
+    while ((bytes_received = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
+        buffer[bytes_received] = '\0';
+        std::cout << buffer << std::endl;
+    }
+    std::cout << "Server closed the connection.\n";
+    close(client_fd);
+    exit(0);
+}
+
+int main() {
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd == -1) {
+        std::cerr << "Error creating socket... exiting";
+        return -1;
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET; // ipv4
+    server_addr.sin_port = htons(SERVER_PORT); // port, converted to big-endian order
+
+    // convert character string to network address https://man7.org/linux/man-pages/man3/inet_pton.3.html
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) == -1){
+        std::cerr << "Error: invalid address\n";
+        return -1;
+    }
+
+    // client does not need to bind, kernel will assign an ip address/port 
+    if (connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
+        std::cerr << "Failed to connect to chat server...";
+        return -1;
+    }
+
+    std::thread receiver_thread(receiveMessages, client_fd);
+    receiver_thread.detach();
+
+    std::string input;
+    std::cout << "Welcome to chat server!" << std::endl;
+    std::cout << "Please enter a username: ";
+    std::getline(std::cin, input);
+    send(client_fd, input.c_str(), input.length(), 0);
+
+    std::cout << "Please enter a chat room to join: ";
+    std::getline(std::cin, input);
+    send(client_fd, input.c_str(), input.length(), 0);
+    
+    while (true) {
+        std::getline(std::cin, input);
+        if (input == "/q") break;
+        send(client_fd, input.c_str(), input.length(), 0);
+    }
+
+    std::cout << "disconnecting...\n";
+    close(client_fd);
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,131 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include <thread>
+
+constexpr u_int16_t PORT = 8080;
+constexpr int MAX_CLIENTS = 10;
+
+struct ClientInfo {
+    int socket_fd;
+    std::string username;
+    std::string room;
+};
+
+std::mutex client_sockets_mutex;
+std::vector<ClientInfo> clients;
+
+
+void broadcastMessage(int sender_fd, const std::string& room, const std::string& message) {
+    client_sockets_mutex.lock();
+    for (const auto& client : clients) {
+        if (client.room == room && client.socket_fd != sender_fd) {
+            //https://man7.org/linux/man-pages/man2/send.2.html
+            send(client.socket_fd, message.c_str(), message.length(), 0);
+        }
+    }
+    client_sockets_mutex.unlock();
+}
+
+
+void handleClient(int client_fd) {
+    char buffer[1024];
+    int bytes_received;
+
+    // https://man7.org/linux/man-pages/man2/recv.2.html
+    bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+    buffer[bytes_received] = '\0'; // null terminate up to bytes received
+    std::string username = buffer;
+
+    bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+    buffer[bytes_received] = '\0';
+    std::string room = buffer;
+
+    ClientInfo new_client = {client_fd, username, room};
+
+    client_sockets_mutex.lock();
+    clients.push_back(new_client);
+    client_sockets_mutex.unlock();
+
+    std::string room_msg = username + " has joined room " + room + "\n";
+    std::cout << room_msg << std::endl;
+    broadcastMessage(client_fd, room, room_msg);
+
+    while ((bytes_received = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
+        buffer[bytes_received] = '\0';
+        std::string msg = username + ": " + std::string(buffer);
+        std::cout << "Room " << room << " | " << msg << std::endl;
+        broadcastMessage(client_fd, room, msg);
+    }
+
+    room_msg = username + " has left room " + room + "\n";
+    broadcastMessage(client_fd, room, room_msg);
+
+    close(client_fd);
+    client_sockets_mutex.lock();
+    
+    clients.erase(std::remove_if(clients.begin(), clients.end(), 
+        [client_fd](const ClientInfo& client) { return client.socket_fd == client_fd; }), 
+        clients.end());
+
+    client_sockets_mutex.unlock();
+
+}
+
+int main() {
+
+    //https://man7.org/linux/man-pages/man2/socket.2.html
+    //https://man7.org/linux/man-pages/man7/ip.7.html
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0); // get file descriptor for new TCP socket
+    if (server_fd == -1) {
+        std::cerr << "error creating socket... exiting";
+        return -1;
+    }
+
+    //https://man7.org/linux/man-pages/man3/sockaddr.3type.html
+    // sockaddr_in specifies socket type is for ip
+    // sin = socket information
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET; // ipv4
+    server_addr.sin_port = htons(PORT); // port, converted to big-endian order
+    server_addr.sin_addr.s_addr = INADDR_ANY; // listen on any network interface
+
+    socklen_t addr_len = sizeof(server_addr);
+
+    //https://man7.org/linux/man-pages/man2/bind.2.html
+    int ret_val = bind(server_fd, (struct sockaddr *) &server_addr, addr_len);
+    if (ret_val == -1) {
+        std::cerr << "error binding socket... exiting";
+        return -1;
+    }
+
+    //https://man7.org/linux/man-pages/man2/listen.2.html
+    listen(server_fd, MAX_CLIENTS);
+    std::cout << "Listening on port " << PORT << "..." << std::endl;
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    while (true) {
+        //https://man7.org/linux/man-pages/man2/accept.2.html
+        int new_client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (new_client_fd == -1) {
+            std::cout << "client accept failed" << std::endl;
+            continue;
+        }
+        char client_ip[INET_ADDRSTRLEN];
+        //https://man7.org/linux/man-pages/man3/inet_ntop.3.html
+        // convert network bytes to string
+        inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+        uint16_t client_port = ntohs(client_addr.sin_port); // get port from client
+        std::cout << "New client connected from " << client_ip << ":" << client_port << "\n";
+
+        std::thread client_thread(handleClient, new_client_fd);
+        client_thread.detach();
+    }
+
+    std::cout << "exiting" << std::endl;
+    close(server_fd);
+    return 0;
+}
